b72c28b2c8e34b9ead09fad7a26be7a7aea36df6
DirupT <iamcooled@gmail.com>
2018-07-27 11:11:12 -0500

    added post to actions

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ server.use(express.json());
 server.post('/api/projects', async (req, res) => {
     const { name, description, completed } = req.body;
     const project = { name, description, completed };
-    if (!name || !description) return res.status(400).json({ error: 'Please provide name, description and completed information.' });
+    if (!name || !description) return res.status(400).json({ error: 'Please provide a name and description.' });
     if (name.length > 128) return res.status(400).json({ error: 'Name provided is too long!' });
     try {
         const response = await projects.insert(project);
@@ -50,7 +50,7 @@ server.put('/api/projects/:id', async (req, res) => {
     const { id } = req.params;
     const { name, description, completed } = req.body;
     const project = { name, description, completed };
-    if (!name || !description) return res.status(400).json({ error: 'Please provide name, description and completed information.' });
+    if (!name || !description) return res.status(400).json({ error: 'Please provide a name and description' });
     if (name.length > 128) return res.status(400).json({ error: 'Name provided is too long!' });
     try {
         const updateResponse = await projects.update(id, project);
@@ -72,6 +72,30 @@ server.delete('/api/projects/:id', async (req, res) => {
 })
 
 // ================================== END OF PROJECTS ================================== //
+
+server.post('/api/actions', async (req, res) => {
+    const { project_id, description, notes, completed } = req.body;
+    const action = { project_id, description, notes, completed };
+    if (!project_id || !description || !notes) return res.status(400).json({ error: 'Please provide project id, description and notes information.' });
+    if (description.length > 128) return res.status(400).json({ error: 'Description provided is too long!' });
+    try {
+        const findResponse = await projects.get();
+        for (let i = 0; i < findResponse.length; i++) {
+            if (findResponse[i].id == project_id) {
+                try {
+                    const response = await actions.insert(action);
+                    return res.status(200).json(response);
+                } catch (err) {
+                    return res.status(500).json({ error: "There was an error while saving the action to the database." });
+                }
+            }
+            return res.status(500).json({ error: "Please provide a project id that matches an existing project's id." })
+        }
+    } catch (err) {
+        return res.status(500).json({ error: "Couldn't retrieve actions information" });
+    }
+})
+
 
 server.get('/api/actions', async (req, res) => {
     try {
