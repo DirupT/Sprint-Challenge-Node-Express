c0608a62dafd6dc039028bfcef25e6e99d6bf4da
DirupT <iamcooled@gmail.com>
2018-07-27 10:33:54 -0500

    added post to projects

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require('express');
 const projects = require('./data/helpers/projectModel');
 
 const server = express();
+server.use(express.json());
+
+server.post('/api/projects', async (req, res) => {
+    const { name, description, completed } = req.body;
+    const project = { name, description, completed };
+    if (!name || !description) return res.status(400).json({ error: 'Please prove name, description and completed information.' });
+    if (name.length > 128) return res.status(400).json({ error: 'Name provided is too long!' });
+    try {
+        const response = await projects.insert(project);
+        return res.status(201).json(response);
+    } catch (err) {
+        return res.status(500).json({ error: 'There was an error while saving the project to the database.' });
+    }
+})
 
 server.get('/api/projects', async (req, res) => {
     try {
