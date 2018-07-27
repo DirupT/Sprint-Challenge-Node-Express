d343b2de85429573cdc58d69bb70f69a362b78e0
DirupT <iamcooled@gmail.com>
2018-07-27 11:15:51 -0500

    added update action by id to actions

--- index.js
+++ index.js
@@ -115,5 +115,29 @@ server.get('/api/actions/:id', async (req, res) => {
     }
 })
 
+server.put('/api/actions/:id', async (req, res) => {
+    const { id } = req.params;
+    const { project_id, description, notes, completed } = req.body;
+    const action = { project_id, description, notes, completed };
+    if (!project_id || !description || !notes) return res.status(400).json({ error: 'Please provide project id, description and notes information.' });
+    if (description.length > 128) return res.status(400).json({ error: 'Description provided is too long!' });
+    try {
+        const findResponse = await projects.get();
+        for (let i = 0; i < findResponse.length; i++) {
+            if (findResponse[i].id == project_id) {
+                try {
+                    const response = await actions.update(id, action);
+                    return res.status(200).json(response);
+                } catch (err) {
+                    return res.status(500).json({ error: "There was an error while saving the action to the database." });
+                }
+            }
+            return res.status(500).json({ error: "Please provide a project id that matches an existing project's id." })
+        }
+    } catch (err) {
+        return res.status(500).json({ error: "Couldn't retrieve actions information" });
+    }
+})
+
 
 server.listen(8000, () => console.log('API is running on port 8000...'));
