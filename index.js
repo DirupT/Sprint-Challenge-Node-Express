f6b9ecd0fb64391218e78151dd58df6a024d11f5
DirupT <iamcooled@gmail.com>
2018-07-27 10:41:55 -0500

    added update by id to projects

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ server.use(express.json());
 server.post('/api/projects', async (req, res) => {
     const { name, description, completed } = req.body;
     const project = { name, description, completed };
-    if (!name || !description) return res.status(400).json({ error: 'Please prove name, description and completed information.' });
+    if (!name || !description) return res.status(400).json({ error: 'Please provide name, description and completed information.' });
     if (name.length > 128) return res.status(400).json({ error: 'Name provided is too long!' });
     try {
         const response = await projects.insert(project);
@@ -32,6 +32,21 @@ server.get('/api/projects/:id', async (req, res) => {
         return res.status(200).json(response);
     } catch (err) {
         return res.status(500).json({ error: "Couldn't retrieve project information." })
+    }
+})
+
+server.put('/api/projects/:id', async (req, res) => {
+    const { id } = req.params;
+    const { name, description, completed } = req.body;
+    const project = { name, description, completed };
+    if (!name || !description) return res.status(400).json({ error: 'Please provide name, description and completed information.' });
+    if (name.length > 128) return res.status(400).json({ error: 'Name provided is too long!' });
+    try {
+        const updateResponse = await projects.update(id, project);
+        if (!updateResponse) return res.status(404).json({ error: "The project with the specified ID does not exist." });
+        return res.status(200).json(updateResponse);
+    } catch (err) {
+        return res.status(500).json({ error: "The project information could not be modified." })
     }
 })
 
