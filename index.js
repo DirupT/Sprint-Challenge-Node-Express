9c645df077fa992c2b4336864a2bdf00c668d78d
DirupT <iamcooled@gmail.com>
2018-07-27 10:44:43 -0500

    added delete by id to projects

--- index.js
+++ index.js
@@ -50,4 +50,14 @@ server.put('/api/projects/:id', async (req, res) => {
     }
 })
 
+server.delete('/api/projects/:id', async (req, res) => {
+    try {
+        const removeResponse = await projects.remove(req.params.id);
+        if (removeResponse === 0) return res.status(404).json({ error: "The project with the specified ID does not exist." });
+        return res.status(200).json(removeResponse);
+    } catch (err) {
+        return res.status(500).json({ error: "The project could not be removed" });
+    }
+})
+
 server.listen(8000, () => console.log('API is running on port 8000...'));
