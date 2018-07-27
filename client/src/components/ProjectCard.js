3b47356718be800e8ef9ae1d72a2d8933b791259
DirupT <iamcooled@gmail.com>
2018-07-27 12:32:34 -0500

    updated styling

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -3,11 +3,15 @@ import React from 'react';
 const ProjectCard = props => {
     return (
         <div className='project-card'>
-            <p><strong>{props.project.name}</strong></p>
+            {!props.details ? <p className='project-title'><strong>{props.project.name}</strong></p> : null}
             {props.details ?
                 <React.Fragment>
-                    <p><strong>Description:</strong> {props.project.description}</p>
-                    <p><strong>Completed:</strong> {String(props.project.completed)}</p>
+                    <div className='project-info'>
+                        <h1>Project Info:</h1>
+                        <p><strong>Name:</strong> {props.project.name}</p>
+                        <p><strong>Description:</strong> {props.project.description}</p>
+                        <p><strong>Completed:</strong> <span style={{ color: props.project.completed ? 'blue' : 'red' }}>{String(props.project.completed)}</span></p>
+                    </div>
                     {props.actions ? props.actions.map(action => {
                         return (
                             <div className='actions-container' key={action.id}>
